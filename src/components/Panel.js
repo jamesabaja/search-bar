8ecd349ae1c53f8879e412f33a541bd8262f3ca9
jgabaja <jgabaja@gmail.com>
2018-06-27 11:20:15 +0800

    deploy

--- src/components/Panel.js
+++ src/components/Panel.js
@@ -4,7 +4,7 @@ const Panel = (props) => {
   return(
     <nav className='panel'>
       <p className='panel-heading'>
-        Search Medicines
+        Search Medicines Module
       </p>
       <div className="panel-block">
         <p className="control has-icons-left">
